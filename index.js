9bc010c5f5348e365809c9e111bfe96e92661532
Lina Suodyte <lsuodyte@gmail.com>
2018-03-13 10:38:21 +0100

    First Commit

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const compression = require('compression');
+const compression = require("compression");
 
 app.use(compression());
 
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV != "production") {
     app.use(
-        '/bundle.js',
-        require('http-proxy-middleware')({
-            target: 'http://localhost:8081/'
+        "/bundle.js",
+        require("http-proxy-middleware")({
+            target: "http://localhost:8081/"
         })
     );
 } else {
-    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+    app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("*", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+    console.log("First commit");
 });
 
 app.listen(8080, function() {
